ea3cdc973da669daff6137db132ef2c778ff0007
NTILinea <linea.petersen@elev.ga.ntig.se>
2022-05-09 16:53:44 +0200

    1.1

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 let btn = document.querySelector(`#reRoll`)
 let shownResault = document.querySelector(`#diceResault`)
 
-
+//dice
 btn.addEventListener(`click`, () => {
     let diceValue = Math.floor((Math.random() * 6) + 1)
-    shownResault.innerText = diceValue
+    shownResault.innerText = diceValue;
+    let dice = document.querySelector('#dice');
+    dice.classList = [];
+    dice.classList.add(`dot-${diceValue}`);
     console.log(diceValue)
+
+    stegenChecker()
 })
+
+//stegen
+let stegenGoal = document.querySelector(`#stegenGoal`)
+
+function stegenChecker() {
+
+    if (diceValue = 1) {
+        console.log("ya got a 1")
+    } else {
+        console.log("nope")
+    }
+}
 
 
 
